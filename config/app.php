7db5b033c0da7a29e196c7d405aa10af165b4475
Olem Babia <obabia@gmail.com>
2017-01-20 10:25:25 +0800

    Added config for SSL connections

--- config/app.php
+++ config/app.php
@@ -155,6 +155,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+		
+		Fideloper\Proxy\TrustedProxyServiceProvider::class,
 
     ],
 
