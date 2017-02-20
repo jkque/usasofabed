927ef055161791bae7470342cca845f32354719e
jkque <johnkevin.cadungog@gmail.com>
2017-02-21 04:55:17 +0800

    aboutus

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -22,17 +22,28 @@
 
 	</section><!-- #page-title end -->
 
-	<!-- Google Map
-	============================================= -->
-	<section id="google-map" class="gmap slider-parallax"></section>
-
 	<!-- Content
 	============================================= -->
 	<section id="content">
 
 		<div class="content-wrap">
 
-			<div class="container clearfix">
+			<div class="container clearfix">					
+
+				<p><span class="dropcap">C</span>apris Furniture was founded  in 1987 by Pedro Interian to manufacture and import furniture for the residential and resort markets. He has been in the furniture business since 1975 in various capacities including manufacturing, sales, purchasing and merchandising. </p>
+				
+				<p>Capris Furniture offers one of the broadest product lines for the residential and resort markets, with a broad selection of living room, occasional, dinning and bedroom furnishings. All the upholstery is manufactured in Capris Furniture’s 1,600 square foot manufacturing facility located in Ocala, Florida. Currently, our manufacturing capacity is over 200 sofas a day.</p>
+
+				<p>In addition to the upholstered goods, a large portion of Capris Furniture’s business consist of importing rattan and wicker from the Far East. These item consist of living room, occasional, dinning, and bedroom furnishings.</p>
+
+				<p>Capris Furniture, through its BeachCraft division, offers an extensive line of quality, environmentally friendly outdoor furniture designed for use of pasios, pool sides, decks and screen enclosures.</p>
+
+				<p>All of our products can be manufactured to meet contract specifications. Capris Furniture has the capability to do custom products to meet individual needs with moderate quantities and lead-time.</p>
+
+				<p>Capris Furniture delivers on its company trucks. When our employees manufacture and deliver your product handling damage is minimized. Delivery in areas not serviced by our company trucks would be arranged through various contract furniture carriers.</p>
+
+				<p>Capris Furniture is also capable of handling container shipments for overseas destinations.Capris Furniture greatly appreciates your consideration for vendor status. Capris Furniture strives to offer a quality product at a fair price and looks forward to the beginning of a long and mutually successful business relationship.</p>
+
 			</div>
 		</div>
 
